18685a7361cfd9aa10b5089109135ae34067a8f5
Vinicius Lepca <vinicius.lepca@gmail.com>
2021-11-27 19:22:10 -0500

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div className="App">
+            <AppNav/>
+            <div style={{margin: 20}}>
+                <div style={{textAlign: "center"}}>
+                    <Introduction/>
+
+                </div>
+                <References/>
+            </div>
+        </div>
+    );
+}
+
+function AppNav() {
+    return <Navbar sticky="top" bg="light" variant="light" style={{paddingLeft: 20}}>
+        <Navbar.Brand href="/">Food Waste in the World</Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link href="#introduction">Introduction</Nav.Link>
+            <Nav.Link href="#analysis1">Analysis 1</Nav.Link>
+            <Nav.Link href="#analysis2">Analysis 2</Nav.Link>
+            <Nav.Link href="#analysis3">Analysis 3</Nav.Link>
+        </Nav>
+    </Navbar>
+}
+
+function Introduction() {
+    return <div id="introduction">
+        <h1>Food Waste in the World</h1>
+        <h5 style={{fontWeight: "normal"}}>Team members: Emma Guo, Kaitlynn Pineda, Vinicius Lepca</h5>
+        <p>Introduction text</p>
     </div>
-  );
+}
+
+function References() {
+    return <div id="references">
+        <h5>References</h5>
+        <ul>
+            <li>Test</li>
+        </ul>
+    </div>
 }
 
 export default App;
