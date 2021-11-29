4d57241bf9e6a4804f701cd401eb11b8c412339b
Vinicius Lepca <vinicius.lepca@gmail.com>
2021-11-29 13:02:34 -0500

    Basic implementation of the world scatter plot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-import {Navbar, Nav} from 'react-bootstrap';
-import React from "react";
+import React from 'react';
+import {Navbar, Nav, Form} from 'react-bootstrap';
+import {
+    Chart as ChartJS,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import {Scatter} from 'react-chartjs-2';
 
 let foodWasteData = require("./food_waste.json");
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
 class App extends React.Component {
     constructor(props) {
@@ -61,15 +71,102 @@ function Introduction() {
 }
 
 class WorldDataVis extends React.Component {
+    constructor(props) {
+        super(props);
+        const earliestYear = 1965;
+        const defaultBeginYear = 1990;
+        const endYear = 2021
+        this.state = {
+            earliestYear: earliestYear,
+            defaultBeginYear: defaultBeginYear,
+            beginYear: defaultBeginYear,
+            endYear: endYear,
+            plotData: {datasets: []},
+            selectedCountry: null
+        }
+    }
+
+    getRandomRgb = () => {
+        const num = Math.round(0xffffff * Math.random());
+        const r = num >> 16;
+        const g = (num >> 8) & 255;
+        const b = num & 255;
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
+
+    generateData = (earliest, latest, countries) => {
+        // Format the data
+        let formatted = {};
+        this.props.data.forEach(item => {
+            const country = item.country;
+            const year = item.year;
+            const loss = item.loss_percentage;
+            // const commodity = item.commodity;
+
+            // Filter by countries
+            if (countries == null || countries.includes(country)) {
+                // Filter by years
+                if (year >= earliest && year <= latest) {
+                    if (formatted.hasOwnProperty(country)) {
+                        formatted[country].data.push({x: year, y: loss})
+                    } else {
+                        formatted[country] = {
+                            label: country,
+                            data: [{x: year, y:loss}],
+                            backgroundColor: this.getRandomRgb()
+                        }
+                    }
+                }
+            }
+        })
+
+        const data = {
+            datasets: Object.values(formatted)
+        }
+
+        // Update the state
+        this.setState({plotData: data})
+    }
+
+    componentDidMount() {
+        this.generateData(this.state.beginYear, this.state.endYear, this.state.selectedCountry);
+    }
+
     render() {
         console.log("Data:", this.props.data);
         console.log("Countries:", this.props.countries);
+        console.log("Plot data", this.state.plotData);
+
+        const options = {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        };
         return (
             <div>
-                Test
+                <YearRangeSlider
+                    earliest={this.state.earliestYear}
+                    latest={this.state.endYear - 1}
+                    defaultValue={this.state.defaultBeginYear}
+                    value={this.state.beginYear}
+                    setValue={value => this.setState({beginYear: value})}
+                />
+                <Scatter option={options} data={this.state.plotData}/>
             </div>
         );
     }
+}
+
+function YearRangeSlider(props) {
+    return (
+        <>
+            <Form.Label>Year Range: {props.value} - 2021</Form.Label><br/>
+            Select start of range:
+            <Form.Range min={props.earliest} max={props.latest} defaultValue={props.defaultValue} onChange={e => props.setValue(e.target.value)}/>
+        </>
+    )
 }
 
 function References() {
