703aec0a565a7f8dda5af3760f5822f7b4ba6b37
Kaitlynn Pineda <kaitlynn@Kaitlynns-MacBook-Pro-2.local>
2021-11-29 23:13:57 -0500

    ave

---
+++ src/country_ave.js
@@ -0,0 +1,598 @@
+export default [
+{
+  text: 'Myanmar',
+  value: 5.24,
+},
+{
+  text: 'Burundi',
+  value: 3.12,
+},
+{
+  text: 'Western Africa',
+  value: 3.75,
+},
+{
+  text: 'Belarus',
+  value: 0.02,
+},
+{
+  text: 'Cambodia',
+  value: 7.48,
+},
+{
+  text: 'Algeria',
+  value: 25.0,
+},
+{
+  text: 'Cameroon',
+  value: 2.89,
+},
+{
+  text: 'Canada',
+  value: 5.7,
+},
+{
+  text: 'Central Asia',
+  value: 14.8,
+},
+{
+  text: 'Sri Lanka',
+  value: 9.83,
+},
+{
+  text: 'Western Asia',
+  value: 14.8,
+},
+{
+  text: 'Chad',
+  value: 3.22,
+},
+{
+  text: 'Northern Africa',
+  value: 14.8,
+},
+{
+  text: 'Europe',
+  value: 3.65,
+},
+{
+  text: 'Chile',
+  value: 6.67,
+},
+{
+  text: 'China',
+  value: 8.58,
+},
+{
+  text: 'China,Taiwan',
+  value: 0.78,
+},
+{
+  text: 'Colombia',
+  value: 7.92,
+},
+{
+  text: 'Democratic Republic of the Congo',
+  value: 2.82,
+},
+{
+  text: 'Costa Rica',
+  value: 6.5,
+},
+{
+  text: 'Cuba',
+  value: 5.59,
+},
+{
+  text: 'Africa',
+  value: 30.0,
+},
+{
+  text: 'Sub-Saharan Africa',
+  value: 7.6,
+},
+{
+  text: 'Benin',
+  value: 4.73,
+},
+{
+  text: 'Denmark',
+  value: 6.78,
+},
+{
+  text: 'Northern America',
+  value: 8.6,
+},
+{
+  text: 'Ecuador',
+  value: 3.97,
+},
+{
+  text: 'El Salvador',
+  value: 7.0,
+},
+{
+  text: 'Ethiopia',
+  value: 2.69,
+},
+{
+  text: 'Eritrea',
+  value: 3.03,
+},
+{
+  text: 'Estonia',
+  value: 1.28,
+},
+{
+  text: 'Angola',
+  value: 2.52,
+},
+{
+  text: 'Fiji',
+  value: 5.99,
+},
+{
+  text: 'Finland',
+  value: 11.33,
+},
+{
+  text: 'France',
+  value: 3.4,
+},
+{
+  text: 'Gabon',
+  value: 35.0,
+},
+{
+  text: 'Georgia',
+  value: 3.38,
+},
+{
+  text: 'Gambia',
+  value: 2.89,
+},
+{
+  text: 'Germany',
+  value: 2.53,
+},
+{
+  text: 'Ghana',
+  value: 5.35,
+},
+{
+  text: 'Azerbaijan',
+  value: 4.82,
+},
+{
+  text: 'Argentina',
+  value: 0.8,
+},
+{
+  text: 'Guatemala',
+  value: 10.11,
+},
+{
+  text: 'Guinea',
+  value: 3.09,
+},
+{
+  text: 'Guyana',
+  value: 12.5,
+},
+{
+  text: 'Haiti',
+  value: 42.5,
+},
+{
+  text: 'Southern Asia',
+  value: 13.2,
+},
+{
+  text: 'Honduras',
+  value: 5.95,
+},
+{
+  text: 'Hungary',
+  value: 1.43,
+},
+{
+  text: 'South-Eastern Asia',
+  value: 13.2,
+},
+{
+  text: 'India',
+  value: 2.86,
+},
+{
+  text: 'Australia',
+  value: 18.4,
+},
+{
+  text: 'Indonesia',
+  value: 5.34,
+},
+{
+  text: 'Iran (Islamic Republic of)',
+  value: 6.54,
+},
+{
+  text: 'Ireland',
+  value: 3.25,
+},
+{
+  text: 'Israel',
+  value: 3.49,
+},
+{
+  text: 'Italy',
+  value: 25.42,
+},
+{
+  text: 'Côte d Ivoire',
+  value: 3.06,
+},
+{
+  text: 'Jamaica',
+  value: 20.34,
+},
+{
+  text: 'Japan',
+  value: 2.9,
+},
+{
+  text: 'Kazakhstan',
+  value: 5.33,
+},
+{
+  text: 'Afghanistan',
+  value: 13.45,
+},
+{
+  text: 'Austria',
+  value: 5.8,
+},
+{
+  text: 'Jordan',
+  value: 6.76,
+},
+{
+  text: 'Kenya',
+  value: 3.54,
+},
+{
+  text: 'Democratic Peoples Republic of Korea',
+  value: 16.32,
+},
+{
+  text: 'Republic of Korea',
+  value: 13.7,
+},
+{
+  text: 'Kyrgyzstan',
+  value: 3.43,
+},
+{
+  text: 'Lao Peoples Democratic Republic',
+  value: 12.12,
+},
+{
+  text: 'Latin America and the Caribbean',
+  value: 14.4,
+},
+{
+  text: 'Lesotho',
+  value: 3.1,
+},
+{
+  text: 'Latvia',
+  value: 0.55,
+},
+{
+  text: 'Liberia',
+  value: 2.37,
+},
+{
+  text: 'Lithuania',
+  value: 3.39,
+},
+{
+  text: 'Luxembourg',
+  value: 7.82,
+},
+{
+  text: 'Madagascar',
+  value: 2.36,
+},
+{
+  text: 'Malawi',
+  value: 2.91,
+},
+{
+  text: 'Malaysia',
+  value: 5.6,
+},
+{
+  text: 'Mali',
+  value: 3.19,
+},
+{
+  text: 'Mauritania',
+  value: 2.97,
+},
+{
+  text: 'Bahrain',
+  value: 25.0,
+},
+{
+  text: 'Mauritius',
+  value: 2.43,
+},
+{
+  text: 'Mexico',
+  value: 16.45,
+},
+{
+  text: 'Mongolia',
+  value: 1.2,
+},
+{
+  text: 'Bangladesh',
+  value: 6.81,
+},
+{
+  text: 'Mozambique',
+  value: 2.73,
+},
+{
+  text: 'Armenia',
+  value: 9.58,
+},
+{
+  text: 'Oman',
+  value: 35.0,
+},
+{
+  text: 'Namibia',
+  value: 2.19,
+},
+{
+  text: 'Nepal',
+  value: 11.69,
+},
+{
+  text: 'Netherlands',
+  value: 1.59,
+},
+{
+  text: 'Australia and New Zealand',
+  value: 42.0,
+},
+{
+  text: 'New Caledonia',
+  value: 1.13,
+},
+{
+  text: 'New Zealand',
+  value: 8.63,
+},
+{
+  text: 'Nicaragua',
+  value: 9.47,
+},
+{
+  text: 'Belgium',
+  value: 6.23,
+},
+{
+  text: 'Niger',
+  value: 2.97,
+},
+{
+  text: 'Nigeria',
+  value: 3.64,
+},
+{
+  text: 'Norway',
+  value: 8.08,
+},
+{
+  text: 'Pakistan',
+  value: 8.49,
+},
+{
+  text: 'Panama',
+  value: 5.0,
+},
+{
+  text: 'Paraguay',
+  value: 11.75,
+},
+{
+  text: 'Peru',
+  value: 21.09,
+},
+{
+  text: 'Philippines',
+  value: 7.26,
+},
+{
+  text: 'Poland',
+  value: 4.1,
+},
+{
+  text: 'Portugal',
+  value: 3.62,
+},
+{
+  text: 'Guinea-Bissau',
+  value: 2.93,
+},
+{
+  text: 'Timor-Leste',
+  value: 3.77,
+},
+{
+  text: 'Romania',
+  value: 2.37,
+},
+{
+  text: 'Russian Federation',
+  value: 2.01,
+},
+{
+  text: 'Rwanda',
+  value: 4.02,
+},
+{
+  text: 'Saint Kitts and Nevis',
+  value: 30.0,
+},
+{
+  text: 'Saint Lucia',
+  value: 16.3,
+},
+{
+  text: 'Bolivia (Plurinational State of)',
+  value: 2.88,
+},
+{
+  text: 'Saudi Arabia',
+  value: 17.29,
+},
+{
+  text: 'Senegal',
+  value: 2.51,
+},
+{
+  text: 'Serbia',
+  value: 1.09,
+},
+{
+  text: 'Sierra Leone',
+  value: 3.21,
+},
+{
+  text: 'Slovakia',
+  value: 3.15,
+},
+{
+  text: 'Viet Nam',
+  value: 8.8,
+},
+{
+  text: 'Somalia',
+  value: 2.71,
+},
+{
+  text: 'South Africa',
+  value: 2.76,
+},
+{
+  text: 'Zimbabwe',
+  value: 2.7,
+},
+{
+  text: 'Botswana',
+  value: 2.65,
+},
+{
+  text: 'Spain',
+  value: 0.42,
+},
+{
+  text: 'South Sudan',
+  value: 3.19,
+},
+{
+  text: 'Sudan',
+  value: 3.16,
+},
+{
+  text: 'Eswatini',
+  value: 3.07,
+},
+{
+  text: 'Sweden',
+  value: 11.32,
+},
+{
+  text: 'Switzerland',
+  value: 1.11,
+},
+{
+  text: 'Brazil',
+  value: 14.25,
+},
+{
+  text: 'Syrian Arab Republic',
+  value: 15.0,
+},
+{
+  text: 'Tajikistan',
+  value: 7.71,
+},
+{
+  text: 'Thailand',
+  value: 3.07,
+},
+{
+  text: 'Togo',
+  value: 3.53,
+},
+{
+  text: 'Trinidad and Tobago',
+  value: 22.94,
+},
+{
+  text: 'Turkey',
+  value: 5.93,
+},
+{
+  text: 'Uganda',
+  value: 2.8,
+},
+{
+  text: 'Ukraine',
+  value: 6.19,
+},
+{
+  text: 'The former Yugoslav Republic of Macedonia',
+  value: 2.2,
+},
+{
+  text: 'Egypt',
+  value: 8.39,
+},
+{
+  text: 'United Kingdom',
+  value: 0.65,
+},
+{
+  text: 'United Kingdom of Great Britain and Northern Ireland',
+  value: 6.17,
+},
+{
+  text: 'United Republic of Tanzania',
+  value: 4.17,
+},
+{
+  text: 'United States of America',
+  value: 11.67,
+},
+{
+  text: 'Burkina Faso',
+  value: 2.58,
+},
+{
+  text: 'Uzbekistan',
+  value: 0.72,
+},
+{
+  text: 'Venezuela (Bolivarian Republic of)',
+  value: 6.94,
+},
+{
+  text: 'Zambia',
+  value: 2.57,
+},
+];
